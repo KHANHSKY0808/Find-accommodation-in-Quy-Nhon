3b0def0a8addd68dbd42a1bfc0af368d9b7af74a
TuanSiu <tuanluu632@gmail.com>
2025-09-25 21:09:05 +0700

    khung project

---
+++ Project/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>© 2025 - Nhóm 4 PHP</p>
+</footer>
+</body>
+</html>
---
+++ Project/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Project PHP</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <h2>Logo / Menu</h2>
+</header>
